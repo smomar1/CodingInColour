bb2cd058767f4efdd0ab8880ec3ea862b7178fda
smomar1 <103393718+smomar1@users.noreply.github.com>
2023-10-10 17:12:02 -0600

    Editing the file to work in node.js setting

--- Lessons/Oct10/tictac.js
+++ Lessons/Oct10/tictac.js
@@ -39,7 +39,13 @@
 
 // End program
 
-const prompt = require('prompt-sync')();
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 //            0    1    2    3    4    5    6    7    8
 let currentPlayer = "X";
